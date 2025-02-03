c4dc18fefa34813913f0ea4862b89f5241f0f089
Benjamin Denozière <benjamin.denoziere@phileogeleven.com>
2025-02-03 17:44:32 +0100

    cleanup

--- monitor/types.ts
+++ monitor/types.ts
@@ -1,7 +1,4 @@
-import { IIRFilter } from "./iir-filter";
 import { KalmanFilter } from "./kalman-filter";
-import { SimpleKalmanFilter } from "./simple-kalman-filter";
-import { RollingDerivate } from "./sg-derivate";
 
 export type Props = {
   filter: KalmanFilter;
